87c2135942333a6fd555339f4427db5e3fd9fa18
Nika <nikax193@gmail.com>
2023-03-11 01:24:11 +0400

    add section carusell, add scrtit for slick, add styles for carusel

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+      speed: 1200,
+      // adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg" alt=""></button>',
+      // responsive: [
+      //   {
+      //     breakpoint: 992,
+      //     settings:{
+      //       dots: true,
+      //       arrows: false
+      //     }
+      //   }
+      // ]
+    });
+  });
